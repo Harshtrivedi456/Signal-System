dc9467d5944f4df1dd8de7a3324b6c7ad9441309
Harsh Trivedi <152944798+Harshtrivedi456@users.noreply.github.com>
2025-08-12 19:57:08 +0530

    Update lan.py

--- lan.py
+++ lan.py
@@ -1,6 +1,7 @@
-om googletrans import LANGUAGES
+from googletrans import LANGUAGES
 
 # Print available languages
 for lang_code, lang_name in LANGUAGES.items():
     print(f"{lang_code}: {lang_name}")
+
 
